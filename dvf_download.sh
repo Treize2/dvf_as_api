13ce400b033f59acbd71751c650fbda6d87f3c79
Adrien Dauchez <adrien@dauchez.me>
2021-04-07 09:50:38 +0200

    Update dvf_download.sh

--- dvf_download.sh
+++ dvf_download.sh
@@ -4,11 +4,13 @@ DEFAULT_MILLESIME=2020-04
 MILLESIME=${1:-$DEFAULT_MILLESIME}
 CQUEST_MILLESIME=$(echo $MILLESIME|sed 's/-//g')
 
+CURRENTYEAR=$(date +'%Y')
+
 mkdir -p data
 cd data
 
 # Téléchargement des fichiers DVF
-for ANNEE in $(seq 2014 2019)
+for ANNEE in $(seq 2014 $CURRENTYEAR)
 do
   if [ $ANNEE -ne 2014 ]
   then
